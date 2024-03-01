b0cf0b2b77d082d3f221e7065171a8aa7f843e1d
kenresoft <amadikenneth8@gmail.com>
2024-03-01 17:47:15 +0100

    Add `clippeer.dart`

---
+++ lib/clippeer.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SemiCircle extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.white12
+      ..style = PaintingStyle.fill;
+
+    final Path path = Path();
+
+    path.moveTo(10, 10);
+    path.arcTo(
+      const Rect.fromLTWH(0, 0, 90, 100),
+      pi,
+      -pi,
+      true,
+    );
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
