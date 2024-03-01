a6ad5434a5424bb556a12a50be6f87147f809a9d
kenresoft <amadikenneth8@gmail.com>
2024-03-01 17:47:53 +0100

    Add drink_collection.dart

--- lib/drink_collection.dart
+++ lib/drink_collection.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wine_delivery_app/clippeer.dart';
 
 class DrinksCollection extends StatelessWidget {
   const DrinksCollection({
@@ -10,54 +11,67 @@ class DrinksCollection extends StatelessWidget {
     required this.color,
   });
 
-  final String name;
-  final String image;
-  final double price;
-  final double rating;
-  final Color color;
+  final List<String> name;
+  final List<String> image;
+  final List<double> price;
+  final List<double> rating;
+  final List<Color> color;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 8,
+      itemCount: 5,
       scrollDirection: Axis.horizontal,
       physics: const BouncingScrollPhysics(),
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.all(4.0).copyWith(left: index == 0 ? 16 : 0, right: index == 7 ? 16 : 0),
-          child: SizedBox(
-            width: 150,
-            height: 200,
-            child: Card(
-              color: color,
-              child: Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Image.asset(
-                      image,
-                      width: 75,
-                      height: 105,
-                    ),
-                  ),
-                  Positioned(
-                    top: 95,
-                    left: 10,
+          child: Stack(children: [
+            Positioned(
+              top: 50,
+              left: 10,
+              child: SizedBox(
+                width: 150,
+                height: 200,
+                child: Card(
+                  color: color[index],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(name, style: const TextStyle(color: Colors.white, fontSize: 19)),
+                        Text(name[index], style: const TextStyle(color: Colors.white, fontSize: 19)),
                         const SizedBox(height: 2),
-                        Text('\$$price', style: const TextStyle(color: Colors.white, fontSize: 16)),
+                        Text('\$${price[index]}', style: const TextStyle(color: Colors.white, fontSize: 16)),
                         const SizedBox(height: 2),
-                        Text(rating.toString(), style: const TextStyle(color: Colors.white)),
+                        Text(rating[index].toString(), style: const TextStyle(color: Colors.white)),
+                        const SizedBox(height: 10),
                       ],
                     ),
                   ),
-                ],
+                ),
               ),
             ),
-          ),
+            Positioned(
+              top: 4,
+              left: 40,
+              child: CustomPaint(
+                painter: SemiCircle(),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Image.asset(
+                  image[index],
+                  height: 155,
+                  width: 140,
+                ),
+              ),
+            ),
+          ]),
         );
       },
     );
