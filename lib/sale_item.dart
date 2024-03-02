8be07e522e8dc84fd28572bb270aac9a059d08be
kenresoft <amadikenneth8@gmail.com>
2024-03-02 11:31:32 +0100

    Modify `sale_item.dart`

--- lib/sale_item.dart
+++ lib/sale_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class SaleItem extends StatelessWidget {
   const SaleItem({
@@ -20,24 +21,24 @@ class SaleItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(children: [
       Positioned(
-        top: 50,
+        top: 70,
         left: 10,
         child: SizedBox(
           width: MediaQuery.of(context).size.width - 20,
-          height: 120,
+          height: 105,
           child: Card(
             color: color,
             child: Padding(
-              padding: const EdgeInsets.only(left: 110, right: 16),
+              padding: const EdgeInsets.only(left: 85, right: 16),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(name, style: const TextStyle(color: Colors.white, fontSize: 19)),
                   const SizedBox(height: 2),
                   Text('\$$price', style: const TextStyle(color: Colors.white, fontSize: 16)),
                   const SizedBox(height: 2),
-                  Text(rating.toString(), style: const TextStyle(color: Colors.white)),
+                  buildRatingBar(rating),
                   const SizedBox(height: 10),
                 ],
               ),
@@ -53,5 +54,22 @@ class SaleItem extends StatelessWidget {
         ),
       ),
     ]);
+  }
+
+  RatingBar buildRatingBar(double value) {
+    return RatingBar.builder(
+      itemSize: 15,
+      initialRating: rating,
+      minRating: 1,
+      direction: Axis.horizontal,
+      allowHalfRating: true,
+      itemCount: 5,
+      unratedColor: Colors.white70,
+      itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+      itemBuilder: (context, _) {
+        return const Icon(Icons.star, size: 2, color: Colors.amber);
+      },
+      onRatingUpdate: (double value) {},
+    );
   }
 }
