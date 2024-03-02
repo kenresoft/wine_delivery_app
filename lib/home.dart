c6ce67176b86117443260530a230ffc7f8f8e272
kenresoft <amadikenneth8@gmail.com>
2024-03-02 11:32:30 +0100

    Modify `home.dart`

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:circular_image/circular_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fontresoft/fontresoft.dart';
 import 'package:wine_delivery_app/sale_item.dart';
@@ -47,13 +48,14 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                               width: 80,
                               top: 10,
                               child: Card(
-                                  color: Colors.grey,
-                                  margin: EdgeInsets.all(10),
-                                  child: Text(
-                                    'Alex',
-                                    style: TextStyle(fontSize: 18, backgroundColor: Colors.transparent),
-                                    textAlign: TextAlign.center,
-                                  )),
+                                color: Colors.grey,
+                                margin: EdgeInsets.all(10),
+                                child: Text(
+                                  'Alex',
+                                  style: TextStyle(fontSize: 18, backgroundColor: Colors.transparent),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
                             ),
                             ClipOval(
                               child: Card(
@@ -74,137 +76,151 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                   ),
                 ),
 
-                const SizedBox(height: 30),
-
                 /// TEXT FIELD
 
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
-                    height: 60,
-                    decoration: const BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(color: Colors.white12, blurRadius: 10, offset: Offset(-2, -2)),
-                        BoxShadow(color: Colors.white12, blurRadius: 10, offset: Offset(2, -10)),
-                      ],
-                    ),
-                    width: MediaQuery.of(context).size.width,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        fillColor: Colors.black12.withOpacity(.03),
-                        filled: true,
-                        hintText: 'Search Drinks',
-                        suffixIcon: const Card(
-                          color: Colors.black,
-                          child: Icon(Icons.search, color: Colors.white),
+                Container(
+                  height: 50,
+                  margin:const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.black12.withOpacity(.03),
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: const [
+                      BoxShadow(color: Colors.white12, blurRadius: 10, offset: Offset(-2, -2)),
+                      BoxShadow(color: Colors.white12, blurRadius: 10, offset: Offset(2, -10)),
+                    ],
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Search Drinks',
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(color: Colors.transparent, width: 1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(color: Colors.transparent, width: 1),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                          ),
                         ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(color: Colors.white12, width: 1),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(color: Colors.white12, width: 1),
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
                       ),
-                    ),
+                       SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: Card(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                          color: Colors.black,
+                          child: const Icon(CupertinoIcons.search, color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
 
                 //SizedBox(height: 10),
 
                 /// TEXT
-                 const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text('Drinks', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'Drinks',
+                    style: Font.poppins(style: const TextStyle(fontSize: 35, fontWeight: FontWeight.w700)),
+                  ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text('Collection', style: TextStyle(fontSize: 30)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('Collection', style: Font.poppins(style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w500))),
                 ),
 
                 const SizedBox(height: 25),
 
-                SizedBox(
-                  height: 35,
-                  child: DefaultTabController(
-                    length: 3,
-                    child: TabBar(
-                      tabs: const [
-                        Text('All'),
-                        Text('Trending'),
-                        Text('Popular'),
-                      ],
-                      controller: tabController,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 35,
+                      width: MediaQuery.of(context).size.width - 80,
+                      child: DefaultTabController(
+                        length: 3,
+                        child: TabBar(
+                          dividerHeight: 0,
+                          indicatorPadding: const EdgeInsets.only(left: 4, right: 4),
+                          indicatorColor: Colors.black,
+                          labelColor: Colors.black,
+                          labelStyle: Font.poppins(style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                          labelPadding: const EdgeInsets.only(top: 5, left: 20, right: 20),
+                          isScrollable: true,
+                          tabAlignment: TabAlignment.start,
+                          tabs: const [
+                            Text('All'),
+                            Text('Trending'),
+                            Text('Popular'),
+                          ],
+                          controller: tabController,
+                        ),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 6, right: 12),
+                      child: Row(
+                        children: [
+                          Text(
+                            '2',
+                            style: Font.poppins(style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                          ),
+                          Text(
+                            '/8',
+                            style: Font.poppins(style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.grey)),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
 
                 const SizedBox(height: 25),
 
                 SizedBox(
                   height: 270,
-                  child: TabBarView(controller: tabController, children: const [
-                    DrinksCollection(
-                      name: [
-                        'Remy Martin',
-                        'Remy Martin',
-                        'Remy Martin',
-                        'Remy Martin',
-                        'Remy Martin',
-                      ],
-                      image: [
-                        'assets/wine-5.png',
-                        'assets/wine-2.png',
-                        'assets/wine-3.png',
-                        'assets/wine-4.png',
-                        'assets/wine-6.png',
-                      ],
-                      price: [
-                        28.45,
-                        28.45,
-                        28.45,
-                        28.45,
-                        28.45,
-                      ],
-                      rating: [
-                        4,
-                        4,
-                        4,
-                        4,
-                        4,
-                      ],
-                      color: [
-                        Color(0xff3e494d),
-                        Color(0xff7E587D),
-                        Color(0xffE18182),
-                        Colors.blueAccent,
-                        Colors.orangeAccent,
-                      ],
-                    ),
-                    DrinksCollection(
-                      name: ['Grover'],
-                      image: ['assets/wine-2.jpg'],
-                      price: [16.99],
-                      rating: [3.0],
-                      color: [Colors.purple],
-                    ),
-                    DrinksCollection(
-                      name: ['Sailor'],
-                      image: ['assets/wine-3.jpg'],
-                      price: [28.45],
-                      rating: [3.5],
-                      color: [Colors.redAccent],
-                    ),
+                  child: TabBarView(controller: tabController, children: [
+                    buildDrinksCollection(),
+                    buildDrinksCollection(),
+                    buildDrinksCollection(),
                   ]),
                 ),
 
                 //const SizedBox(height: 5),
 
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text('On Sale', style: TextStyle(fontSize: 30)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        'On Sale',
+                        style: Font.poppins(style: const TextStyle(fontSize: 28, fontWeight: FontWeight.w500)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 6, right: 12),
+                      child: Row(
+                        children: [
+                          Text(
+                            '1',
+                            style: Font.poppins(style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                          ),
+                          Text(
+                            '/3',
+                            style: Font.poppins(style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.grey)),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
 
                 const SizedBox(height: 5),
@@ -214,7 +230,6 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                   child: ListView.builder(
                     itemCount: 4,
                     itemExtent: MediaQuery.of(context).size.width,
-                    shrinkWrap: true,
                     physics: const BouncingScrollPhysics(),
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) {
@@ -222,39 +237,57 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                         name: 'Carpene Malvolti Prosecco',
                         image: 'assets/wine-6.png',
                         price: 17.40,
-                        rating: 7,
+                        rating: 3,
                         color: Color(0xffC37D7D),
                       );
                     },
                   ),
                 ),
-
-                /*Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Stack(
-                    children: [
-                      const SizedBox(
-                        width: double.infinity,
-                        height: 120,
-                        child: Card(color: Color(0xffC37D7D)),
-                      ),
-                      Positioned(
-                        left: 20,
-                        bottom: 50,
-                        child: Image.asset(
-                          'assets/wine.png',
-                          height: 70,
-                          width: 70,
-                        ),
-                      )
-                    ],
-                  ),
-                )*/
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  DrinksCollection buildDrinksCollection() {
+    return const DrinksCollection(
+      name: [
+        'Remy Martin',
+        'Grover',
+        'Sailor',
+        'Remy Martin',
+        'Grover',
+      ],
+      image: [
+        'assets/wine-5.png',
+        'assets/wine-2.png',
+        'assets/wine-3.png',
+        'assets/wine-4.png',
+        'assets/wine-6.png',
+      ],
+      price: [
+        28.45,
+        16.99,
+        16.99,
+        28.45,
+        28.45,
+      ],
+      rating: [
+        4,
+        3,
+        3,
+        2,
+        5,
+      ],
+      color: [
+        Color(0xff3e494d),
+        Color(0xff7E587D),
+        Color(0xffE18182),
+        Colors.blue,
+        Colors.orange,
+      ],
     );
   }
 }
