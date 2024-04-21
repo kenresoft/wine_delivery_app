f43f7f09668c2346d67338da145a665227a82c3f
kenresoft <amadikenneth8@gmail.com>
2024-04-21 23:59:46 +0100

    Add cart_item.dart

---
+++ lib/model/cart_item.dart
@@ -0,0 +1,11 @@
+class CartItem {
+  final String itemName;
+  final double itemPrice;
+  final int quantity;
+
+  CartItem({
+    required this.itemName,
+    required this.itemPrice,
+    required this.quantity,
+  });
+}
