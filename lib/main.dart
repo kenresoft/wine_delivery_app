4635f8df3288434fa55ac43026e879882c52acb2
kenresoft <amadikenneth8@gmail.com>
2024-04-21 20:39:37 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fontresoft/fontresoft.dart';
 import 'package:wine_delivery_app/page_1/home.dart';
 import 'package:wine_delivery_app/page_2/product_page.dart';
@@ -13,21 +14,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-        useMaterial3: true,
-        fontFamily: FontResoft.poppins,
-        textTheme: const TextTheme(bodyMedium: TextStyle(color: Color(0xff252525))),
-        package: FontResoft.package,
+    return ScreenUtilInit(
+      designSize: const Size(448, 998),
+      minTextAdapt: false,
+      ensureScreenSize: false,
+      useInheritedMediaQuery: true,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+          useMaterial3: true,
+          fontFamily: FontResoft.poppins,
+          textTheme: const TextTheme(bodyMedium: TextStyle(color: Color(0xff252525))),
+          package: FontResoft.package,
+        ),
+        routes: {
+          '/': (context) => const Home(),
+          'product_page': (context) => const ProductPage(),
+        },
+        //home: const Home(),
       ),
-      routes: {
-        '/': (context) => const Home(),
-        'product_page': (context) => const ProductPage(),
-      },
-      //home: const Home(),
     );
   }
 }
