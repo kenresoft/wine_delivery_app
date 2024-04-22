e4e0389c47138b027cccb96cbfe3e3479f9b8360
kenresoft <amadikenneth8@gmail.com>
2024-04-22 15:39:03 +0100

    Update order_item.dart model

---
+++ lib/model/order_item.dart
@@ -0,0 +1,11 @@
+class OrderItem {
+  final String itemName;
+  final double itemPrice;
+  final int quantity;
+
+  OrderItem({
+    required this.itemName,
+    required this.itemPrice,
+    required this.quantity,
+  });
+}
