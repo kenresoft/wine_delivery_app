749e11e00823ccaa7db7175ef2487f87e47d3029
kenresoft <amadikenneth8@gmail.com>
2024-04-22 15:41:19 +0100

    Update utils.dart

---
+++ lib/utils/utils.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+extension ToastExtension on String {
+  void get toast {
+    Fluttertoast.showToast(
+      msg: toString(),
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.black54,
+      textColor: Colors.white,
+      fontSize: 18,
+    );
+  }
+
+  void toasts(BuildContext context) {
+    FToast().init(context).showToast(
+          gravity: ToastGravity.BOTTOM,
+          toastDuration: const Duration(seconds: 8),
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff383838),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            //constraints: BoxConstraints(maxWidth: .8.sw),
+            padding: const EdgeInsets.all(10).r,
+            child: FittedBox(
+              //constrainedAxis: Axis.vertical,
+              child: SizedBox(
+                width: .6.sw,
+                child: Row(
+                  children: [
+                    const Icon(CupertinoIcons.check_mark, color: Colors.green),
+                    Text(
+                      this,
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: true,
+                      style: TextStyle(color: Colors.white, fontSize: 15.sp),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+  }
+}
