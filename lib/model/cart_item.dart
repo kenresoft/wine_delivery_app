962ad93921adfff0a5c1349d6be1521b0ac88171
kenresoft <amadikenneth8@gmail.com>
2024-04-23 12:40:58 +0100

    Update cart_item.dart

--- lib/model/cart_item.dart
+++ lib/model/cart_item.dart
@@ -1,11 +1,15 @@
 class CartItem {
   final String itemName;
   final double itemPrice;
-  final int quantity;
+  int quantity;
+  final String imageUrl; // New field for drink image URL
+  double purchaseCost; // New field for purchase cost
 
   CartItem({
     required this.itemName,
     required this.itemPrice,
     required this.quantity,
+    required this.imageUrl,
+    required this.purchaseCost,
   });
 }
