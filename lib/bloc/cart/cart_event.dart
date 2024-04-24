4d4c0a37c5038d47f518effba3f811463bdf80bd
kenresoft <amadikenneth8@gmail.com>
2024-04-24 08:40:11 +0100

    Implement cart_event.dart

---
+++ lib/bloc/cart/cart_event.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+import '../../model/cart_item.dart';
+
+abstract class CartEvent extends Equatable {
+  const CartEvent();
+}
+
+class AddToCartEvent extends CartEvent {
+  final CartItem cartItem;
+
+  const AddToCartEvent(this.cartItem);
+
+  @override
+  List<Object?> get props => [cartItem];
+}
+
+class RemoveFromCartEvent extends CartEvent {
+  final String itemName;
+
+  const RemoveFromCartEvent(this.itemName);
+
+  @override
+  List<Object?> get props => [itemName];
+}
