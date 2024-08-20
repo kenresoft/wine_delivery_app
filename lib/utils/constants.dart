59b1c0317b4a0277e087c91b6a0bc270e04e810d
kenresoft <amadikenneth8@gmail.com>
2024-08-20 17:44:33 +0100

    Update constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  Constants._();
+
+  static const empty = '';
+  static const unknownDevice = 'Unknown device';
+  static const String baseUrl = 'http://192.168.67.207:3333';
+  //static const String baseUrl2 = 'http://192.168.1.207/';
+
+}
