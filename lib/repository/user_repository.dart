b6d94ca15c1cf154781be81918d6666b38e72ce0
kenresoft <amadikenneth8@gmail.com>
2024-08-25 20:34:39 +0100

    Update user_repository.dart

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:http_parser/http_parser.dart';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
@@ -54,6 +55,64 @@ class UserRepository {
         throw 'Failed to retrieve user information (error code: 404). \n'
             'Please check your internet connection and try again. \n'
             'If the issue persists, contact our support team at [email protected].';
+      }
+      rethrow;
+    }
+  }
+
+  static const String _updateUrl = '${Constants.baseUrl}/api/users/';
+
+  // New Method to Update User Profile
+  Future<Profile> updateUserProfile({
+    required String userId,
+    required Map<String, String> updatedData,
+    File? profileImage, // Optional profile image
+  }) async {
+    final token = await authRepository.getToken();
+
+    try {
+      final uri = Uri.parse('$_updateUrl/$userId');
+      var request = http.MultipartRequest('PUT', uri)
+        ..headers['Authorization'] = 'Bearer $token';
+
+      // Add updated fields to the request
+      updatedData.forEach((key, value) {
+        request.fields[key] = value;
+      });
+
+      // If there's a profile image, add it to the request
+      if (profileImage != null) {
+        request.files.add(await http.MultipartFile.fromPath(
+          'profileImage',
+          profileImage.path,
+          contentType: MediaType('image', 'jpeg'), // Adjust based on the image type
+        ));
+      }
+
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final userJson = data['user'];
+
+        if (userJson != null) {
+          return Profile.fromJson(userJson);
+        }
+        throw 'Error parsing updated user data from the server.';
+      }
+      if (response.statusCode == 401) {
+        throw 'Unauthorized: Please log in again.';
+      }
+      throw 'Error updating user profile: ${response.statusCode} - ${response.reasonPhrase}';
+    } catch (e) {
+      if (kDebugMode) {
+        print(e.toString());
+      }
+      if (e is SocketException) {
+        throw 'Failed to update user information. \n'
+            'Please check your internet connection and try again. \n'
+            'If the issue persists, contact our support team.';
       }
       rethrow;
     }
