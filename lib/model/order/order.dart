3eb2888e28b69d5c2ac05eff1f73761a3bb6a1a4
kenresoft <amadikenneth8@gmail.com>
2024-09-05 22:45:30 +0100

    Update project

--- lib/model/order/order.dart
+++ lib/model/order/order.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:equatable/equatable.dart';
 
 part 'order_item.dart';
@@ -45,4 +46,79 @@ class Order extends Equatable {
 
   @override
   bool get stringify => true; // Set stringify to true for better debugging
+}
+*/
+
+part 'order_item.dart';
+
+part '../enums/order_status.dart';
+
+class Order {
+  final String id;
+  final String userId;
+  final List<OrderItem> items;
+  final String shipmentId;
+  final int subTotal;
+  final int totalCost;
+  final String status;
+  final String createdAt;
+  final PaymentDetails paymentDetails;
+
+  Order({
+    required this.id,
+    required this.userId,
+    required this.items,
+    required this.shipmentId,
+    required this.subTotal,
+    required this.totalCost,
+    required this.status,
+    required this.createdAt,
+    required this.paymentDetails,
+  });
+
+  factory Order.fromJson(Map<String, dynamic> json) {
+    return Order(
+      id: json['_id'],
+      userId: json['user'],
+      items: (json['items'] as List).map((item) => OrderItem.fromJson(item)).toList(),
+      shipmentId: json['shipment'],
+      subTotal: json['subTotal'],
+      totalCost: json['totalCost'],
+      status: json['status'],
+      createdAt: json['createdAt'],
+      paymentDetails: PaymentDetails.fromJson(json['paymentDetails']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'user': userId,
+      'items': items.map((item) => item.toJson()).toList(),
+      'shipment': shipmentId,
+      'subTotal': subTotal,
+      'totalCost': totalCost,
+      'status': status,
+      'createdAt': createdAt,
+      'paymentDetails': paymentDetails.toJson(),
+    };
+  }
+}
+
+class PaymentDetails {
+  final String paymentIntent;
+
+  PaymentDetails({required this.paymentIntent});
+
+  factory PaymentDetails.fromJson(Map<String, dynamic> json) {
+    return PaymentDetails(
+      paymentIntent: json['paymentIntent'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'paymentIntent': paymentIntent,
+    };
+  }
 }
--- lib/model/order/order_item.dart
+++ lib/model/order/order_item.dart
@@ -1,15 +1,29 @@
 part of 'order.dart';
 
 class OrderItem {
-  final String itemName;
-  final double itemPrice;
+  final String productId;
   final int quantity;
-  final String imageUrl;
+  final String id;
 
   OrderItem({
-    required this.itemName,
-    required this.itemPrice,
+    required this.productId,
     required this.quantity,
-    required this.imageUrl,
+    required this.id,
   });
+
+  factory OrderItem.fromJson(Map<String, dynamic> json) {
+    return OrderItem(
+      productId: json['product'],
+      quantity: json['quantity'],
+      id: json['_id'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'product': productId,
+      'quantity': quantity,
+      '_id': id,
+    };
+  }
 }
