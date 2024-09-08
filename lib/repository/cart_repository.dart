440e5bd02e5eef4ad3f39c0027405cf0825bb2e3
kenresoft <amadikenneth8@gmail.com>
2024-09-08 14:47:58 +0100

    Update project

--- lib/repository/cart_repository.dart
+++ lib/repository/cart_repository.dart
@@ -1,71 +1,252 @@
-import '../model/cart_item.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:wine_delivery_app/model/cart_item.dart';
+import 'package:wine_delivery_app/repository/auth_repository.dart';
+import 'package:wine_delivery_app/repository/product_repository.dart';
+import 'package:wine_delivery_app/utils/constants.dart';
+
+import '../utils/utils.dart';
 
 class CartRepository {
   CartRepository._();
 
   static final CartRepository _instance = CartRepository._();
 
-  // Getter for the singleton instance
   static CartRepository getInstance() {
     return _instance;
   }
 
-  List<CartItem> cartItems = [];
+  static const String _baseUrl = '${Constants.baseUrl}/api/cart';
 
-  void addToCart(String itemName, double itemPrice, String imageUrl, int quantity, double purchaseCost) {
-    // Check if the item already exists in the cart
-    bool itemExists = false;
-    for (var item in cartItems) {
-      if (item.itemName == itemName) {
-        itemExists = true;
-        break;
+  Future<List<CartItem>> getCartItems() async {
+    // final token = await authRepository.getAccessToken();
+
+    try {
+      final response = await makeRequest(_baseUrl);
+
+      return await parseCartResponse(response);
+    } catch (e) {
+      if (e is SocketException) {
+        throw 'Failed to fetch cart items. Please check your internet connection and try again.';
       }
-    }
-
-    if (!itemExists) {
-      cartItems.add(CartItem(
-        itemName: itemName,
-        itemPrice: itemPrice,
-        quantity: quantity,
-        imageUrl: imageUrl,
-        purchaseCost: purchaseCost,
-      ));
+      throw 'Failed to fetch cart items: ${e.toString()}';
     }
   }
 
-  void removeFromCart(String itemName) {
-    cartItems.removeWhere((item) => item.itemName == itemName);
-  }
+  Future<List<CartItem>> addToCart(String productId, int quantity) async {
+    final token = await authRepository.getAccessToken();
 
-  void increaseQuantity(int index) {
-    cartItems[index] = cartItems[index].copyWith(quantity: cartItems[index].quantity + 1);
-  }
+    try {
+      final response = await http.post(
+        Uri.parse('$_baseUrl/add'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode({'productId': productId, 'quantity': quantity}),
+      );
 
-  void decreaseQuantity(int index) {
-    if (cartItems[index].quantity > 1) {
-      cartItems[index] = cartItems[index].copyWith(quantity: cartItems[index].quantity - 1);
+      return await parseCartResponse(response);
+
+      /*if (response.statusCode != 200) {
+        throw 'Error adding to cart: ${response.statusCode} - ${response.reasonPhrase}';
+      }*/
+    } catch (e) {
+      throw 'Failed to add to cart: ${e.toString()}';
     }
   }
 
-  double getTotalPrice() {
-    double totalPrice = 0;
-    for (var item in cartItems) {
-      totalPrice += item.itemPrice * item.quantity;
-    }
-    return totalPrice;
-  }
+  Future<void> updateCartItem(String itemId, int quantity) async {
+    final token = await authRepository.getAccessToken();
 
-  double calculatePurchaseCost() {
-    Set<String> uniqueItems = <String>{}; // Store unique item names
-    double totalPurchaseCost = 0;
-    for (var item in cartItems) {
-      if (!uniqueItems.contains(item.itemName)) {
-        totalPurchaseCost += item.purchaseCost;
-        uniqueItems.add(item.itemName); // Add item name to unique set
+    try {
+      final response = await http.put(
+        Uri.parse('$_baseUrl/$itemId'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode({'quantity': quantity}),
+      );
+
+      if (response.statusCode != 200) {
+        throw 'Error updating cart item: ${response.statusCode} - ${response.reasonPhrase}';
       }
+    } catch (e) {
+      throw 'Failed to update cart item: ${e.toString()}';
     }
-    return totalPurchaseCost;
+  }
+
+  Future<List<CartItem>> removeFromCart(String itemId) async {
+    final token = await authRepository.getAccessToken();
+
+    try {
+      final response = await http.delete(
+        Uri.parse('$_baseUrl/$itemId'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      return await parseCartResponse(response);
+
+      /*if (response.statusCode != 200) {
+        throw 'Error removing from cart: ${response.statusCode} - ${response.reasonPhrase}';
+      }*/
+    } catch (e) {
+      throw 'Failed to remove from cart: ${e.toString()}';
+    }
+  }
+
+  Future<List<CartItem>> removeAllFromCart() async {
+    final token = await authRepository.getAccessToken();
+
+    try {
+      final response = await http.delete(
+        Uri.parse('$_baseUrl/'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      return await parseCartResponse(response);
+
+      /*if (response.statusCode != 200) {
+        throw 'Error removing from cart: ${response.statusCode} - ${response.reasonPhrase}';
+      }*/
+    } catch (e) {
+      throw 'Failed to remove from cart: ${e.toString()}';
+    }
+  }
+
+  Future<List<CartItem>> incrementCartItem(String itemId) async {
+    try {
+      final response = await makeRequest('$_baseUrl/increment/$itemId', method: RequestMethod.put);
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final cartItemsJson = data['cart']['items'] as List<dynamic>;
+
+        // Fetch all products in parallel
+        List<CartItem> cartItems = await Future.wait(cartItemsJson.map((itemJson) async {
+          final cart = Cart.fromJson(itemJson);
+          final product = await productRepository.getProductById(cart.product!);
+
+          return CartItem(
+            id: cart.id,
+            quantity: cart.quantity,
+            product: product,
+          );
+        }).toList());
+
+        print(DateTime.now());
+        return cartItems;
+      } else {
+        throw Exception('Error fetching cart items: ${response.statusCode} - ${response.reasonPhrase}');
+      }
+
+      /*if (response.statusCode != 200) {
+        throw 'Error incrementing cart item: ${response.statusCode} - ${response.reasonPhrase}';
+      }*/
+    } catch (e) {
+      throw 'Failed to increment cart item: ${e.toString()}';
+    }
+  }
+
+  Future<List<CartItem>> decrementCartItem(String itemId) async {
+    final token = await authRepository.getAccessToken();
+
+    try {
+      final response = await http.put(
+        Uri.parse('$_baseUrl/decrement/$itemId'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      return await parseCartResponse(response);
+
+      /*if (response.statusCode != 200) {
+        throw 'Error decrementing cart item: ${response.statusCode} - ${response.reasonPhrase}';
+      }*/
+    } catch (e) {
+      throw 'Failed to decrement cart item: ${e.toString()}';
+    }
+  }
+
+  Future<double> getTotalPrice({String? couponCode}) async {
+    final token = await authRepository.getAccessToken();
+
+    try {
+      final response = await http.post(
+        Uri.parse('$_baseUrl/price'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode({'couponCode': couponCode}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return double.parse(data['totalPrice'].toString());
+      } else {
+        throw 'Error fetching total price: ${response.statusCode} - ${response.reasonPhrase}';
+      }
+    } catch (e) {
+      throw 'Failed to fetch total price: ${e.toString()}';
+    }
+  }
+
+  Future<bool> isProductInCart(String productId) async {
+    final token = await authRepository.getAccessToken();
+
+    try {
+      final response = await http.get(
+        Uri.parse('$_baseUrl/isProductInCart/$productId'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return data['productInCart'];
+      } else {
+        throw 'Error checking if product is in cart: ${response.statusCode} - ${response.reasonPhrase}';
+      }
+    } catch (e) {
+      throw 'Failed to check if product is in cart: ${e.toString()}';
+    }
+  }
+
+  Future<List<CartItem>> parseCartResponse(http.Response response) async {
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final cartItemsJson = data['cart']['items'] as List<dynamic>;
+
+      // Fetch all products in parallel
+      List<CartItem> cartItems = await Future.wait(cartItemsJson.map((itemJson) async {
+        final cartItem = Cart.fromJson(itemJson);
+        final product = await productRepository.getProductById(cartItem.product!);
+
+        return CartItem(
+          id: cartItem.id,
+          quantity: cartItem.quantity,
+          product: product,
+        );
+      }).toList());
+
+      return cartItems;
+    } else {
+      throw Exception('Error fetching cart items: ${response.statusCode} - ${response.reasonPhrase}');
+    }
   }
 }
 
-final CartRepository cartManager = CartRepository.getInstance();
+CartRepository cartRepository = CartRepository.getInstance();
--- lib/repository/product_repository.dart
+++ lib/repository/product_repository.dart
@@ -4,14 +4,13 @@ import 'package:http/http.dart' as http;
 
 import '../model/product.dart';
 import '../utils/constants.dart';
-import '../utils/prefs.dart';
+import '../utils/utils.dart';
 
 class ProductRepository {
   ProductRepository._();
 
   static final ProductRepository _instance = ProductRepository._();
 
-  // Getter for the singleton instance
   static ProductRepository getInstance() {
     return _instance;
   }
@@ -19,43 +18,60 @@ class ProductRepository {
   ProductRepository();
 
   Future<List<Product>> getAllProducts() async {
-    //final token = await authRepository.getToken();
-    print(authToken);
-    final response = await http.get(
-      Uri.parse('${Constants.baseUrl}/api/products'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $authToken',
-      },
-    );
 
     try {
+      final response = await makeRequest('${Constants.baseUrl}/api/products');
+
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body)['products'];
         return data.map((productJson) => Product.fromJson(productJson)).toList();
       } else {
         //throw Exception('Failed to load products');
-        print(response.toString());
         return [];
       }
-    } on Exception catch (e) {
-      print(e.toString());
+    } on Exception {
       return [];
+    }
+  }
+
+  Future<List<Product>> getProductsByIds(List<String> ids) async {
+    try {
+      // Join the list of IDs with a comma
+      final idsParam = ids.join(',');
+      final response = await makeRequest('${Constants.baseUrl}/api/products/ids/$idsParam');
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        // Check if 'products' key exists in the response
+        if (data['products'] != null) {
+          final productsList = data['products'] as List<dynamic>;
+
+          // Map each product JSON object to a Product model
+          final products = productsList.map((productData) {
+            return Product.fromJson(productData as Map<String, dynamic>);
+          }).toList();
+
+          return products;
+        } else {
+          throw 'Error: "products" key not found in response.';
+        }
+      }
+
+      if (response.statusCode == 401) {
+        throw 'Unauthorized: Please log in again.';
+      }
+
+      throw 'Error fetching user favorites: ${response.statusCode} - ${response.reasonPhrase}';
+    } catch (e) {
+      throw 'ERROR: $e';
     }
   }
 
   Future<Product> getProductById(String id) async {
     try {
-      final response = await http.get(
-        Uri.parse('${Constants.baseUrl}/api/products/$id'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $authToken',
-        },
-      );
+      final response = await makeRequest('${Constants.baseUrl}/api/products/$id');
       if (response.statusCode == 200) {
-        print(json.decode(response.body)['product']);
-        // final p = Product.fromJson(json.decode(response.body)['product']);
         final data = jsonDecode(response.body);
         if (data['product'] != null) {
           final productData = data['product'] as Map<String, dynamic>;
@@ -63,7 +79,6 @@ class ProductRepository {
         } else {
           throw 'Error: Product not found in response.';
         }
-        // return Product.fromJson(json.decode(response.body)['product']);
       }
 
       if (response.statusCode == 401) {
@@ -77,18 +92,16 @@ class ProductRepository {
   }
 
   Future<Product> createProduct(Product product) async {
-    final response = await http.post(
-      Uri.parse('${Constants.baseUrl}/api/products'),
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode({
-        'name': product.name,
-        'category': product.category.id,
-        'image': product.image,
-        'price': product.price,
-        'alcoholContent': product.alcoholContent,
-        'description': product.description,
-      }),
-    );
+    final response = await makeRequest('${Constants.baseUrl}/api/products',
+        method: RequestMethod.post,
+        body: json.encode({
+          'name': product.name,
+          'category': product.category.id,
+          'image': product.image,
+          'price': product.price,
+          'alcoholContent': product.alcoholContent,
+          'description': product.description,
+        }));
 
     if (response.statusCode == 201) {
       return Product.fromJson(json.decode(response.body)['product']);
@@ -127,4 +140,4 @@ class ProductRepository {
   }
 }
 
-final ProductRepository productManager = ProductRepository.getInstance();
+final ProductRepository productRepository = ProductRepository.getInstance();
