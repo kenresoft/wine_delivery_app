2cb54d4f2c226be7dc070f99775aed9a911b0609
kenresoft <amadikenneth8@gmail.com>
2024-09-09 13:57:12 +0100

    Update project

--- android/app/src/main/kotlin/com/kenresoft/wine_delivery_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/kenresoft/wine_delivery_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.kenresoft.wine_delivery_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
