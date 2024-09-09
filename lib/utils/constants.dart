692bd16fdb109eca0230de19558056d3108ec5f3
kenresoft <amadikenneth8@gmail.com>
2024-09-09 14:09:07 +0100

    Update project

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -8,4 +8,7 @@ class Constants {
   static const String baseUrl = 'http://192.168.6.207:3333';
 
   static const int tokenRefreshThreshold = 60;
+
+  static const String stripePublishableKey = 'pk_test_51PtmJH1oKmbRUnqpSrtIE00IY4c4AQGqNO9QZcyWnIx7p6btNWAzvyX23XIzd2OLXXXxSldi2gDiOMwejUx4irrl00OVMw69O2';
+
 }
