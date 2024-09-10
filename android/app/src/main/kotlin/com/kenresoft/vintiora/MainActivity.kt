ed860893977524a60e8e1cf728379e0d27a274d4
kenresoft <amadikenneth8@gmail.com>
2024-09-10 18:24:42 +0100

    Update project

--- android/app/src/main/kotlin/com/kenresoft/vintiora/MainActivity.kt
+++ android/app/src/main/kotlin/com/kenresoft/vintiora/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.kenresoft.vintiora
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
