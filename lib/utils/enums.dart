b9fb4637513d0ed18d5bfff98c70a5d41bba9596
kenresoft <amadikenneth8@gmail.com>
2024-09-12 07:17:40 +0100

    Update project

---
+++ lib/utils/enums.dart
@@ -0,0 +1,32 @@
+enum OrderStatus {
+  draft,
+  pending,
+  processing,
+  packaging,
+  shipping,
+  delivered,
+  cancelled,
+}
+
+enum WineCategory {
+  red,
+  white,
+  rose,
+  sparkling,
+  dessert,
+  fortified,
+  organic,
+  biodynamic,
+  natural,
+  kosher,
+  lowSulfur,
+  nonAlcoholic,
+  vegan,
+}
+
+enum RequestMethod {
+  get,
+  post,
+  put,
+  delete,
+}
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,74 +1,21 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
-import 'package:wine_delivery_app/utils/app_exception.dart';
-import 'package:wine_delivery_app/utils/app_theme.dart';
+import 'package:wine_delivery_app/utils/exceptions.dart';
 import 'package:wine_delivery_app/views/auth/login_page.dart';
 import 'package:wine_delivery_app/views/home/main_screen.dart';
 
 import '../repository/auth_repository.dart';
 import 'constants.dart';
+import 'enums.dart';
 
-extension ToastExtension<T> on T {
-  T get toast {
-    Fluttertoast.showToast(
-      msg: toString(),
-      toastLength: Toast.LENGTH_LONG,
-      gravity: ToastGravity.SNACKBAR,
-      backgroundColor: AppTheme().themeData.colorScheme.primary,
-      // backgroundColor: Colors.black54,
-      textColor: Colors.white,
-      fontSize: 18,
-    );
-    return this;
-  }
-
-  void toasts(BuildContext context) {
-    FToast().init(context).showToast(
-          gravity: ToastGravity.BOTTOM,
-          toastDuration: const Duration(seconds: 8),
-          child: Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff383838),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            //constraints: BoxConstraints(maxWidth: .8.sw),
-            padding: const EdgeInsets.all(10).r,
-            child: FittedBox(
-              //constrainedAxis: Axis.vertical,
-              child: SizedBox(
-                width: .6.sw,
-                child: Row(
-                  children: [
-                    const Icon(CupertinoIcons.check_mark, color: Colors.green),
-                    Text(
-                      toString(),
-                      maxLines: 5,
-                      overflow: TextOverflow.ellipsis,
-                      softWrap: true,
-                      style: TextStyle(color: Colors.white, fontSize: 15.sp),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-  }
-}
-
-/*
 class Utils {
   Utils._();
-*/
 
 /// API Request Wrapper
-Future<http.Response> makeRequest(
-  String endpoint, {
-  RequestMethod method = RequestMethod.get,
-  Map<String, String>? headers,
+  static Future<http.Response> makeRequest(
+    String endpoint, {
+    RequestMethod method = RequestMethod.get,
+    Map<String, String>? headers,
     dynamic body,
   }) async {
     String accessToken = await authRepository.getAccessToken();
@@ -86,10 +33,10 @@ Future<http.Response> makeRequest(
     return response;
   }
 
-Future<http.Response> _getResponse(
-  RequestMethod method,
-  String endpoint,
-  Map<String, String> headers,
+  static Future<http.Response> _getResponse(
+    RequestMethod method,
+    String endpoint,
+    Map<String, String> headers,
     body,
   ) async {
     return switch (method) {
@@ -100,10 +47,10 @@ Future<http.Response> _getResponse(
     };
   }
 
-Future<void> authCheck(BuildContext context) async {
-  const String endpoint = '${Constants.baseUrl}/api/categories';
-  try {
-    final response = await authRepository.makeAuthenticatedRequest(endpoint);
+  static Future<void> authCheck(BuildContext context) async {
+    const String endpoint = '${Constants.baseUrl}/api/categories';
+    try {
+      final response = await authRepository.makeAuthenticatedRequest(endpoint);
       if (response.statusCode == 200) {
       Navigator.of(context).push(
         MaterialPageRoute(
@@ -122,23 +69,44 @@ Future<void> authCheck(BuildContext context) async {
           ),
         );
       }
-  } on NoAccessTokenException {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) {
-          return const LoginPage();
-        },
-      ),
-    );
-  } catch (e) {
-    print(e);
+    } on NoAccessTokenException catch (e) {
+      print(e.message);
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return const LoginPage();
+          },
+        ),
+      );
+    } on NoRefreshTokenException catch (e) {
+      print(e.message);
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return const LoginPage();
+          },
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
   }
-}
-/*}*/
 
-enum RequestMethod {
-  get,
-  post,
-  put,
-  delete,
+  // Handle errors based on status code
+  static AppException handleError(http.Response response) {
+    switch (response.statusCode) {
+      case 400:
+        return const BadRequestException('Bad request, please check your input.');
+      case 401:
+        return const InvalidAccessTokenException('Invalid access token: Please log in again.');
+      case 403:
+        return const UnauthorizedException('Unauthorized: Access Denied.');
+      case 404:
+        return const NotFoundException('Resource not found.');
+      case 500:
+        return const ServerErrorException('Internal server error, please try again later.');
+      default:
+        return UnexpectedException('Unexpected error: ${response.statusCode} - ${response.reasonPhrase}');
+    }
+  }
 }
