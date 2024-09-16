318ff902f29ef54d6d5b8ef058ccfd90bf5222cb
kenresoft <amadikenneth8@gmail.com>
2024-09-16 13:10:36 +0100

    Update project

--- lib/repository/cache_repository.dart
+++ lib/repository/cache_repository.dart
@@ -1,33 +1,25 @@
 import 'package:extensionresoft/extensionresoft.dart';
 import 'package:wine_delivery_app/utils/extensions.dart';
 
-import 'cache.dart';
-
-class CacheRepository implements Cache<String> {
+class CacheRepository {
   CacheRepository._();
 
   static final CacheRepository _instance = CacheRepository._();
 
   static CacheRepository getInstance() => _instance;
 
-  @override
   Future<void> cache(String key, String? body) async {
-    //final orderJsonList = orders.map((order) => jsonEncode(order.toJson())).toList();
     await SharedPreferencesService.setString(key, body ?? '');
   }
 
-  @override
   Future<String> getCache(String key) async {
     return SharedPreferencesService.getString(key) ?? '';
-    //return cachedOrders.map((orderJson) => Order.fromJson(jsonDecode(orderJson))).toList();
   }
 
-  @override
   Future<bool> hasCache(String key) async {
     return SharedPreferencesService.containsKey(key) && !(await getCache(key)).isNullOrEmpty;
   }
 
-  @override
   Future<void> clearCache(String key) async {
     await SharedPreferencesService.remove(key);
   }
