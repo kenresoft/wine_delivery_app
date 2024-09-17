1ddbd134035f2b36d92a1e7358a61613d3641e94
kenresoft <amadikenneth8@gmail.com>
2024-09-17 03:39:00 +0100

    Update order_repository.dart and storage_repository.dart

---
+++ lib/repository/decision_repository_backup.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:wine_delivery_app/repository/cache_repository.dart';
+import 'package:wine_delivery_app/utils/enums.dart';
+
+import '../utils/utils.dart';
+
+class DecisionRepository {
+  final CacheRepository _cacheRepository;
+
+  DecisionRepository._(this._cacheRepository);
+
+  /// Decides whether to fetch data from the cache or the API, processes the result
+  /// via [onSuccess] or handles errors via [onError].
+  Future<T> decide<T>({
+    required String cacheKey,
+    required String endpoint,
+    RequestMethod requestMethod = RequestMethod.get,
+    dynamic body,
+    required Future<T> Function(dynamic data) onSuccess,
+    required Future<T> Function(dynamic error) onError,
+  }) async {
+    try {
+      // Check if valid cache exists
+      if (!_cacheRepository.hasCache(cacheKey)) {
+        // No cache, fetch from API
+        final response = await Utils.makeRequest(endpoint, method: requestMethod, body: body);
+
+        if (response.statusCode == 200) {
+          print('Data fetched from API and cached.');
+          await _cacheRepository.cache(cacheKey, response.body);
+        } else {
+          print('API request failed with status: ${response.statusCode}');
+          await _cacheRepository.cache(cacheKey, null); // Optional: cache empty or null result
+          return onError(Utils.handleError(response));
+        }
+      }
+
+      // Retrieve cached data
+      final cachedData = _cacheRepository.getCache(cacheKey);
+      if (cachedData != null) {
+        try {
+          final decodedData = jsonDecode(cachedData);
+          return onSuccess(decodedData);
+        } catch (e) {
+          // Handle decoding error
+          print('Error decoding cached data: $e');
+          return onError(e);
+          // return await _fetchData(endpoint); // Or handle error gracefully
+        }
+      } else {
+        // Fallback: No valid cache, refetch from API
+        final response = await Utils.makeRequest(endpoint, method: requestMethod, body: body);
+
+        if (response.statusCode == 200) {
+          print('Data refetched from API and cached.');
+          await _cacheRepository.cache(cacheKey, response.body);
+          return onSuccess(jsonDecode(response.body));
+        } else {
+          print('Failed to fetch data from API after cache miss.');
+          return onError(Utils.handleError(response));
+        }
+      }
+    } on Exception catch (e) {
+      print('Exception occurred: $e');
+      return onError(e);
+    }
+  }
+}
+
+final DecisionRepository decisionRepository = DecisionRepository._(cacheRepository);
