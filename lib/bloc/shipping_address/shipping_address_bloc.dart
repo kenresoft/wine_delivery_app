879a671ad5e2bea9c421d48abd73e8bcf9c9322a
kenresoft <amadikenneth8@gmail.com>
2024-10-03 17:37:42 +0100

    Update shipment bloc

--- lib/bloc/shipping_address/shipping_address_bloc.dart
+++ lib/bloc/shipping_address/shipping_address_bloc.dart
@@ -1,9 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:wine_delivery_app/utils/countries.dart';
+import 'package:wine_delivery_app/repository/shipment_repository.dart';
 
 part 'shipping_address_event.dart';
-
 part 'shipping_address_state.dart';
 
 class ShippingAddressBloc extends Bloc<ShippingAddressEvent, ShippingAddressState> {
@@ -11,7 +10,7 @@ class ShippingAddressBloc extends Bloc<ShippingAddressEvent, ShippingAddressStat
     on<ShippingAddressEvent>((event, emit) async {
       emit(ShippingAddressLoading());
       try {
-        final countriesState = await loadCountriesWithStates();
+        final countriesState = await shipmentRepository.loadCountriesWithStates();
         emit(ShippingAddressLoaded(countriesState: countriesState));
       } catch (e) {
         emit(ShippingAddressError());
