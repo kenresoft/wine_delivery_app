9db7925e702169745caace35615023ac910ce751
kenresoft <amadikenneth8@gmail.com>
2024-10-07 15:40:49 +0100

    Update quantity_button.dart

---
+++ lib/views/product/cart/quantity_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class QuantityButton extends StatelessWidget {
+  final Function() onPressed;
+  final IconData icon;
+
+  const QuantityButton({
+    super.key,
+    required this.onPressed,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      padding: EdgeInsets.zero,
+      constraints: BoxConstraints(minWidth: 25, minHeight: 25),
+      style: ButtonStyle(
+        shape: WidgetStatePropertyAll(CircleBorder()),
+        backgroundColor: WidgetStatePropertyAll(Theme.of(context).primaryColorDark),
+        // backgroundColor: WidgetStatePropertyAll(Colors.grey.shade300),
+        iconSize: WidgetStatePropertyAll(20),
+        iconColor: WidgetStatePropertyAll(Theme.of(context).canvasColor),
+      ),
+      icon: Icon(icon),
+    );
+  }
+}
