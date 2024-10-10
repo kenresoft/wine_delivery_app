9398ce477bbad8bd736c4c7e82b4426b5e6b3e0c
kenresoft <amadikenneth8@gmail.com>
2024-10-10 12:24:41 +0100

    Update themes.dart

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -18,15 +18,32 @@ class AppTheme {
     }
   }
 
-  ThemeData get themeData {
+  get border {
+    return RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8),
+    );
+  }
+
+  ColorScheme get colorScheme {
     final brightness = isDarkMode ? Brightness.dark : Brightness.light;
+    return ColorScheme.fromSeed(
+      seedColor: seedColor,
+      brightness: brightness,
+    ).copyWith(
+      primary: secondaryColor,
+      primaryContainer: surfaceTintColor,
+      secondary: secondaryColor,
+      tertiary: tertiaryColor,
+      surface: surfaceColor,
+      surfaceTint: surfaceTintColor,
+    );
+  }
+
+  ThemeData get themeData {
 
     return ThemeData(
-      brightness: brightness,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: primaryColor,
-        brightness: brightness, // Ensure ColorScheme uses the same brightness
-      ),
+      brightness: colorScheme.brightness,
+      colorScheme: colorScheme,
       primaryColor: primaryColor,
       scaffoldBackgroundColor: scaffoldBackgroundColor,
       fontFamily: 'Poppins',
@@ -36,35 +53,35 @@ class AppTheme {
         ),
       ),
       appBarTheme: AppBarTheme(
-        backgroundColor: Colors.transparent,
+        // backgroundColor: Colors.transparent,
         elevation: 0,
+        backgroundColor: colorScheme.surface,
+        foregroundColor: colorScheme.onSurface,
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarIconBrightness: isDarkMode ? Brightness.light : Brightness.dark,
         ),
+      ),
+      listTileTheme: ListTileThemeData(
+        shape: border,
+        selectedColor: colorScheme.secondary,
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           foregroundColor: buttonTextColor, // Use record for button text color
           backgroundColor: primaryColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
+          shape: border,
         ),
       ),
       textButtonTheme: TextButtonThemeData(
         style: TextButton.styleFrom(
           foregroundColor: primaryColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
+          shape: border,
         ),
       ),
       outlinedButtonTheme: OutlinedButtonThemeData(
         style: OutlinedButton.styleFrom(
           foregroundColor: secondaryColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
+          shape: border,
           side: BorderSide(
             color: secondaryColor,
           ),
@@ -100,49 +117,107 @@ class AppTheme {
         ),
         elevation: 0,
       ),
+      tabBarTheme: TabBarTheme(
+        labelColor: colorScheme.secondary,
+        unselectedLabelColor: colorScheme.onSurfaceVariant,
+        indicator: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: colorScheme.secondary,
+              width: 2,
+            ),
+          ),
+        ),
+      ),
+      bottomAppBarTheme: BottomAppBarTheme(
+        color: colorScheme.surface,
+        elevation: 0,
+      ),
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor: scaffoldBackgroundColor,
-        selectedItemColor: primaryColor,
-        unselectedItemColor: secondaryColor,
-        selectedIconTheme: IconThemeData(
-          color: primaryColor,
-        ),
-        unselectedIconTheme: IconThemeData(
-          color: secondaryColor,
-        ),
-        selectedLabelStyle: TextStyle(
-          color: primaryColor,
-        ),
-        unselectedLabelStyle: TextStyle(
-          color: secondaryColor,
-        ),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: colorScheme.surfaceContainerHighest,
+        selectedItemColor: colorScheme.onSurface,
+        unselectedItemColor: colorScheme.onSurfaceVariant,
+        elevation: 0,
+        landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
         showUnselectedLabels: true,
       ),
     );
   }
 
   // Define a record to hold light and dark colors for easy access
-  final (Color light, Color dark) primaryColors = (Color(0xff394346), Color(0xffBD7879));
-  final (Color light, Color dark) scaffoldBackgroundColors = (Color(0xFFFAF9F6), Color(0xFF141414));
-  final (Color light, Color dark) textColors = (Color(0xff394346), Color(0xffF5F5F5));
-  final (Color light, Color dark) secondaryColors = (Color(0xffB3BDC0), Color(0xff687A80));
-  final (Color light, Color dark) surfaceTintColors = (Color(0xffF4F4F4), Color(0xff465760));
-  final (Color light, Color dark) buttonTextColors = (Colors.white, Colors.black);
-  final (Color light, Color dark) cardColors = (Color(0xFFF5F5F5), Color(0xff212121));
+  static const seedColors = (
+    rootColor,
+    baseColor,
+  );
+
+  static const primaryColors = (
+    Color(0xff394346),
+    baseColor,
+  );
+
+  static const secondaryColors = (
+    Color(0xffB3BDC0),
+    Color(0xff687A80),
+  );
+
+  static const tertiaryColors = (
+    Color(0xFFBD7879),
+    Color(0xff794F50),
+  );
+
+  static const surfaceColors = (
+    Color(0xFFF4F4F4),
+    Color(0xFF394346),
+  );
+
+  static const surfaceTintColors = (
+    Color(0xffF4F4F4),
+    Color(0xff465760),
+  );
+
+  static const scaffoldBackgroundColors = (
+    Color(0xFFFAF9F6),
+    Color(0xFF141414),
+  );
+
+  static const textColors = (
+    Color(0xff394346),
+    Color(0xffF5F5F5),
+  );
+
+  static const buttonTextColors = (
+    Colors.white,
+    Colors.black,
+  );
+
+  static const cardColors = (
+    Color(0xFFF5F5F5),
+    Color(0xff212121),
+  );
 
   // Use getters to access the correct color based on isDarkMode
+  Color get seedColor => isDarkMode ? seedColors.$2 : seedColors.$1;
+
   Color get primaryColor => isDarkMode ? primaryColors.$2 : primaryColors.$1;
+
+  Color get secondaryColor => isDarkMode ? secondaryColors.$2 : secondaryColors.$1;
+
+  Color get tertiaryColor => isDarkMode ? tertiaryColors.$2 : tertiaryColors.$1;
+
+  Color get surfaceColor => isDarkMode ? surfaceColors.$2 : surfaceColors.$1;
+
+  Color get surfaceTintColor => isDarkMode ? surfaceTintColors.$2 : surfaceTintColors.$1;
 
   Color get scaffoldBackgroundColor => isDarkMode ? scaffoldBackgroundColors.$2 : scaffoldBackgroundColors.$1;
 
   Color get textColor => isDarkMode ? textColors.$2 : textColors.$1;
 
-  Color get secondaryColor => isDarkMode ? secondaryColors.$2 : secondaryColors.$1;
-
-  Color get surfaceTintColor => isDarkMode ? surfaceTintColors.$2 : surfaceTintColors.$1;
-
   Color get buttonTextColor => isDarkMode ? buttonTextColors.$2 : buttonTextColors.$1;
 
   Color get cardColor => isDarkMode ? cardColors.$2 : cardColors.$1;
+
+  static const Color baseColor = Color(0xffBD7879);
+  static const Color rootColor = Color(0xff7d557a);
 }
 
