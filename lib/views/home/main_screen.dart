11e8a6f4c851522d7caa221e9d59c07be399a010
kenresoft <amadikenneth8@gmail.com>
2024-11-06 23:35:34 +0100

    Update main_screen.dart

--- lib/views/home/main_screen.dart
+++ lib/views/home/main_screen.dart
@@ -1,20 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wine_delivery_app/views/home/home.dart';
-import 'package:wine_delivery_app/views/home/home_screen.dart';
-import 'package:wine_delivery_app/views/user/user_profile_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:wine_delivery_app/utils/extensions.dart';
 
 import '../../bloc/navigation/bottom_navigation_bloc.dart';
-import '../product/cart/shopping_cart.dart';
+import '../../utils/globals.dart';
+import '../../utils/routes/nav.dart';
+import '../cart/shopping_cart.dart';
+import '../category/products_category_screen.dart';
+import '../order/order_history.dart';
+import '../user/user_profile_screen.dart';
 import 'app_exit_dialog.dart';
+import 'home_screen.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   final List<Widget> _pages = const [
     HomeScreen(),
-    Home(),
+    // Home(),
+    CategoryScreen(),
     ShoppingCartScreen(),
+    OrderHistoryScreen(),
     UserProfileScreen(),
   ];
 
@@ -22,67 +32,74 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<NavigationBloc, NavigationState>(
       builder: (context, state) {
-        int currentIndex = 0;
+        /*int currentIndex = 0;
         if (state is PageChanged) {
           currentIndex = state.selectedIndex;
-        }
+        }*/
 
         return Scaffold(
           body: PopScope(
             canPop: false,
             onPopInvokedWithResult: (didPop, result) {
-              if (currentIndex != 0) {
+              /*if (state.selectedIndex != 0) {
                 BlocProvider.of<NavigationBloc>(context).add(const PageTapped(0));
-              } else {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (BuildContext context) {
-                    return AppExitDialog(
-                      title: 'Exit App?',
-                      message: 'Are you sure you want to exit the app?',
+              } else {*/
+              showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (BuildContext context) {
+                  return AppExitDialog(
+                    title: 'Exit App?',
+                    message: 'Are you sure you want to exit the app?',
                       confirmButtonText: 'Exit',
                       cancelButtonText: 'Cancel',
                       onConfirm: () {
-                        // Handle exit logic
-                        Navigator.pop(context);
-                      },
-                      onCancel: () {
-                        // Handle cancel logic
+                      Nav.pop();
+                      if (isAndroid) {
+                        SystemNavigator.pop();
+                      } else if (isIOS) {
+                        exit(0);
+                      }
+                    },
+                    onCancel: () {
                         Navigator.pop(context);
                       },
                     );
                   },
                 );
-              }
+              // }
             },
-            child: IndexedStack(
+            child: _pages[state.selectedIndex],
+            /*child: IndexedStack(
               index: currentIndex,
               children: _pages,
-            ),
+            ),*/
           ),
           bottomNavigationBar: BottomNavigationBar(
-            items: const [
+            items: [
               BottomNavigationBarItem(
-                icon: Icon(Icons.home),
+                icon: FontAwesomeIcons.house.ic,
                 label: 'Home',
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                label: 'Favorites',
+                icon: FontAwesomeIcons.magnifyingGlass.ic,
+                label: 'Search',
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart),
+                icon: FontAwesomeIcons.cartShopping.ic,
                 label: 'Cart',
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.person),
+                icon: FontAwesomeIcons.cartFlatbed.ic,
+                label: 'Orders',
+              ),
+              BottomNavigationBarItem(
+                icon: FontAwesomeIcons.solidUser.ic,
                 label: 'Profile',
               ),
             ],
-            currentIndex: currentIndex,
-            selectedItemColor: Colors.amber[800],
-            type: BottomNavigationBarType.fixed,
+            currentIndex: state.selectedIndex,
+            // selectedItemColor: Colors.amber[800],
             onTap: (index) {
               BlocProvider.of<NavigationBloc>(context).add(PageTapped(index));
             },
