ad54180b878a0530ec57f740cd67d9b4a070fc74
kenresoft <amadikenneth8@gmail.com>
2025-03-11 21:14:30 +0100

    Update plugin dependencies and add file_picker, sqflite, and video_player plugins
    This commit updates multiple plugin dependencies in `pubspec.yaml`, including:
    - `cupertino_icons`
    - `font_awesome_flutter`
    - `flutter_secure_storage`
    - `dio`
    - `socket_io_client`
    - `flutter_stripe`
    - `firebase_core`
    - `firebase_messaging`
    - `path_provider`
    - `image_picker`
    - `permission_handler`
    - `flutter_local_notifications`
    - `share_plus`
    - `app_links`
    - `fluttertoast`
    - `rename_app`
    - `change_app_package_name`
    - `extensionresoft`
    - `flutter_bloc`
    - `hydrated_bloc`
    - `equatable`
    
    It also introduces new plugins to `GeneratedPluginRegistrant.swift`:
    - `file_picker`
    - `sqflite_darwin`
    - `video_player_avfoundation`
    
    Additionally this commit add `devtools_options.yaml` file for flutter devtools settings.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import app_links
 import connectivity_plus
+import file_picker
 import file_selector_macos
 import firebase_core
 import firebase_messaging
@@ -15,10 +16,13 @@ import flutter_secure_storage_macos
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
+import sqflite_darwin
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
@@ -27,4 +31,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
