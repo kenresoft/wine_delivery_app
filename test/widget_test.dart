bc13aaf5eeaee102d17d08ea6fd8109718a815af
kenresoft <amadikenneth8@gmail.com>
2025-03-11 21:16:12 +0100

    Update widget_test.dart and remove pref_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wine_delivery_app/main.dart';
+import 'package:vintiora/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
