77cf5a8ee9318e1087a9ee17c5bd110edda641b2
kenresoft <amadikenneth8@gmail.com>
2025-03-17 15:03:56 +0100

    Update auth entities, repositories, and usecases
    - Add Equatable to AuthTokens
    - Update imports and error handling in auth usecases and repositories.

--- lib/features/auth/domain/entities/auth_tokens.dart
+++ lib/features/auth/domain/entities/auth_tokens.dart
@@ -1,9 +1,14 @@
-class AuthTokens {
+import 'package:equatable/equatable.dart';
+
+class AuthTokens extends Equatable {
   final String accessToken;
   final String refreshToken;
 
-  AuthTokens({
+  const AuthTokens({
     required this.accessToken,
     required this.refreshToken,
   });
+
+  @override
+  List<Object?> get props => [accessToken, refreshToken];
 }
--- lib/features/auth/domain/usecases/register_usecase.dart
+++ lib/features/auth/domain/usecases/register_usecase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
+import 'package:vintiora/core/error/failures.dart';
 
-import '../../core/failures/failures.dart';
 import '../repositories/auth_repository.dart';
 
 class RegisterUseCase {
