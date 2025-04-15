a776607d107ebf44e6fc0ac130ba4c3ce4203e29
kenresoft <amadikenneth8@gmail.com>
2025-04-16 00:00:03 +0100

    Refactor: Update config loading and event handling
    This commit refactors the application configuration loading and event handling processes in `app_config.dart`.  Specifically:
    
    -   Renames the `done` callback in `Config.load` to `onComplete` for clarity.
    -   Improves error handling in `Config.load` by logging exceptions with stack traces and rethrowing them for upstream handling.
    -   Introduces `Config.loadMainEvents` to load essential events (product, flash sales, profile) separately.
    -   Modifies `Config.loadEvents` to optionally load only the main events or all events (including favorites, shipment, and cart) based on the `loadOnlyMainEvents` flag.  The default behavior remains loading all events.
    -   Adds a check for context mount before initiating bloc operations in `_waitForBlocCompletion` to prevent errors.
    -   Ensures that the `completer` is completed only once in `_waitForBlocCompletion`.
    -   Improves error handling in `simulateError` by logging exceptions with stack traces and rethrowing.
    -   Updates logging messages for clarity.
    -   Replaces the simultaneous execution of `_waitForBlocCompletion` calls in `loadMainEvents` and `loadEvents` with `Future.wait` for better concurrency management.

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:extensionresoft/extensionresoft.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,7 +18,7 @@ import 'package:vintiora/core/utils/constants.dart';
 import 'package:vintiora/core/utils/firebase_util.dart';
 import 'package:vintiora/core/utils/notification_util.dart';
 import 'package:vintiora/core/utils/utils.dart';
-import 'package:vintiora/features/cart/presentation/bloc/cart/cart_bloc.dart';
+import 'package:vintiora/features/cart/presentation/bloc/cart_old/cart_bloc.dart';
 import 'package:vintiora/features/flash_sale/presentation/blocs/active_flash_sales/active_flash_sales_bloc.dart';
 import 'package:vintiora/features/order/presentation/bloc/shipment/shipment_bloc.dart';
 import 'package:vintiora/features/product/presentation/bloc/favorite/favs_bloc.dart';
@@ -26,7 +27,7 @@ import 'package:vintiora/features/user/presentation/bloc/profile/profile_bloc.da
 import 'package:vintiora/firebase_options.dart';
 
 class Config {
-  static Future<void> load({void Function(dynamic error)? done}) async {
+  static Future<void> load({void Function(dynamic error)? onComplete}) async {
     try {
       // Initialize dependencies concurrently where possible
       await Hive.initFlutter();
@@ -34,19 +35,18 @@ class Config {
         EnvironmentConfig.load(ConfigMode.dev),
         _initializeStorage(),
         LocalStorage.init(Constants.dbName),
-        // _requestPermissions(), //TODO: uncomment
+        // _requestPermissions(), //TODO: uncomment when needed
         _initNotification(),
       ]);
 
       Stripe.publishableKey = Constants.stripePublishableKey;
       setupDependencies();
 
-      // await simulateError();
-
-      done?.call(null);
-    } catch (e) {
-      // logger.e('Error loading config: $e');
-      done?.call(e);
+      onComplete?.call(null);
+    } catch (e, stackTrace) {
+      logger.e('Error loading config', error: e, stackTrace: stackTrace);
+      onComplete?.call(e);
+      rethrow;
     }
   }
 
@@ -75,10 +75,11 @@ class Config {
     ].request();
   }
 
-  static Future<void> loadEvents([BuildContext? buildContext]) async {
-    BuildContext context = buildContext ?? navContext!;
+  static Future<void> loadMainEvents([BuildContext? buildContext]) async {
+    final context = buildContext ?? navContext!;
+    if (!context.mounted) return;
 
-    final List<Future<void>> blocOperations = [
+    await Future.wait([
       _waitForBlocCompletion<ProductBloc, ProductState>(
         context: context,
         action: (bloc) => bloc.add(LoadAllProducts()),
@@ -99,34 +100,43 @@ class Config {
         action: (bloc) => bloc.add(LoadActiveFlashSales()),
         condition: (state) => state.status != ActiveFlashSalesStatus.loading,
       ),
-
-      //
       _waitForBlocCompletion<ProfileBloc, ProfileState>(
         context: context,
         action: (bloc) => bloc.add(const ProfileFetch()),
         condition: (state) => state.status != ProfileStatus.loading,
       ),
-      _waitForBlocCompletion<FavsBloc, FavsState>(
-        context: context,
-        action: (bloc) => bloc.add(LoadFavs()),
-        condition: (state) => state != null,
-      ),
-      _waitForBlocCompletion<ShipmentBloc, ShipmentState>(
-        context: context,
-        action: (bloc) => bloc.add(GetShipmentDetails()),
-        condition: (state) => state != null,
-      ),
-      _waitForBlocCompletion<CartBloc, CartState>(
-        context: context,
-        action: (bloc) => bloc.add(GetCartItems()),
-        condition: (state) => state.status != CartStatus.loading,
-      ),
-    ];
+    ]);
+  }
 
-    blocOperations.addAll([]);
+  static Future<void> loadEvents({BuildContext? context, bool loadOnlyMainEvents = false}) async {
+    final ctx = context ?? navContext!;
+    if (!ctx.mounted) return;
 
-    // Wait for all bloc operations to complete
-    await Future.wait(blocOperations);
+    // Load events based on the flag
+    if (loadOnlyMainEvents) {
+      await loadMainEvents();
+    } else {
+      await Future.wait([
+        loadMainEvents(ctx),
+        _waitForBlocCompletion<FavsBloc, FavsState>(
+          context: ctx,
+          action: (bloc) => bloc.add(LoadFavs()),
+          condition: (state) => state != null,
+        ),
+        _waitForBlocCompletion<ShipmentBloc, ShipmentState>(
+          context: ctx,
+          action: (bloc) => bloc.add(GetShipmentDetails()),
+          condition: (state) => state != null,
+        ),
+        _waitForBlocCompletion<CartBloc, CartState>(
+          context: ctx,
+          action: (bloc) => bloc.add(GetCartItems()),
+          condition: (state) => state.status != CartStatus.loading,
+        ),
+      ]);
+    }
+
+    logger.w('All config events loaded');
   }
 
   // Helper method to wait for a bloc operation to complete
@@ -134,7 +144,9 @@ class Config {
     required BuildContext context,
     required void Function(B bloc) action,
     required bool Function(S state) condition,
-  }) {
+  }) async {
+    if (!context.mounted) return;
+
     final completer = Completer<void>();
     final bloc = BlocProvider.of<B>(context);
     late StreamSubscription<S> subscription;
@@ -142,16 +154,23 @@ class Config {
     subscription = bloc.stream.listen((state) {
       if (condition(state)) {
         subscription.cancel();
-        completer.complete();
+        if (!completer.isCompleted) {
+          completer.complete();
+        }
       }
     });
 
     action(bloc);
-    return completer.future;
+    await completer.future;
   }
 
   static Future<void> simulateError() async {
-    final file = File('non_existent_file.txt');
-    await file.readAsString();
+    try {
+      final file = File('non_existent_file.txt');
+      await file.readAsString();
+    } catch (e, stackTrace) {
+      logger.e('Simulated error', error: e, stackTrace: stackTrace);
+      rethrow;
+    }
   }
 }
